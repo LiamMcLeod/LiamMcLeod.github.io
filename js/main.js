55166b314c4b9f94ed870f9db9481be575d53e86
dual1tyx <dual1tyx@hotmail.co.uk>
2016-02-22 18:33:50 +0000

    First Commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+ $(document).ready(function () {
+
+    //$(window).scroll(function(){
+    //    var windowTop = $(window).scrollTop();
+    //    var divTop = $('nav').offset().top();
+    //
+    //    if (windowTop > divTop) {
+    //        $("nav").addClass("navbar-fixed-bottom");
+    //    } else {
+    //        $("nav").addClass("navbar-fixed-bottom");
+    //    }
+    //
+    // });
+    //
+    // $("nav a").click(function(evn){
+    //     evn.preventDefault();
+    //     $('html,body').scrollTo(this.hash, this.hash);
+    // });
+    //
+    // var aChildren = $("nav li").children(); // find the a children of the list items
+    // var aArray = []; // create the empty aArray
+    // for (var i=0; i < aChildren.length; i++) {
+    //     var aChild = aChildren[i];
+    //     var ahref = $(aChild).attr('href');
+    //     aArray.push(ahref);
+    // }
+    //
+    // $(window).scroll(function(){
+    //     var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+    //     var windowHeight = $(window).height(); // get the height of the window
+    //     var docHeight = $(document).height();
+    //
+    //     for (var i=0; i < aArray.length; i++) {
+    //         var theID = aArray[i];
+    //         var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+    //         var divHeight = $(theID).height(); // get the height of the div in question
+    //         if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+    //             $("a[href='" + theID + "']").addClass("active");
+    //         } else {
+    //             $("a[href='" + theID + "']").removeClass("active");
+    //         }
+    //     }
+    //
+    //     if(windowPos + windowHeight == docHeight) {
+    //         if (!$("nav li:last-child a").hasClass("active")) {
+    //             var navActiveCurrent = $(".nav-active").attr("href");
+    //             $("a[href='" + navActiveCurrent + "']").removeClass("active");
+    //             $("nav li:last-child a").addClass("active");
+    //         }
+    //     }
+    // });
+     //http://callmenick.com/post/single-page-site-with-smooth-scrolling-highlighted-link-and-fixed-navigation
+
+
+
+     //$("section").mouseenter(function(){
+     //    var id = $(this).attr('id');
+     //    $('a').removeClass('active');
+     //    $("[href=\"#"+id+"\"]").addClass('active');
+     //});
+
+});
